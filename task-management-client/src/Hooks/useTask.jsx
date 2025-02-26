42fc7855d391af2c3d7b06d0498a1a31ae01546d
AsadEducation <AsadEducation@outlook.com>
2025-02-26 21:24:59 +0600

    loaded data from server

--- task-management-client/src/Hooks/useTask.jsx
+++ task-management-client/src/Hooks/useTask.jsx
@@ -1,13 +1,15 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import useAxiosSecure from "./useAxiosSecure";
 
 
 export const useTask = () => {
 
     const [allTask, setAllTask] = useState();
+    const axios = useAxiosSecure();
 
     useEffect(() => {
-        axios.get('/public/Fake/task.json')
+        axios.get('/tasks')
             .then(result => {
                 setAllTask(result.data)
                 // console.log(allTask)
